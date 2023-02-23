098ddb64bfdb886a1661d2a71f7c190b04b3e06b
Dixzen <akshatdixit2017@gmail.com>
2023-02-23 15:30:19 +0530

    subtraction function

---
+++ sub.js
@@ -0,0 +1,7 @@
+// Taking input from the user
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+
+// subtracting two numbers
+let diff = num1 - num2;
+console.log("The difference of " + num1 + " and " + num2 + " is: " + diff);
