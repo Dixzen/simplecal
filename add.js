c5b8282572563c0448dacf009c7b8ea45c1ab49d
Palak807 <palak.sahu20@st.niituniversity.in>
2023-02-23 15:20:49 +0530

    Initial Commit Palak

---
+++ add.js
@@ -0,0 +1,7 @@
+// Taking input from the user
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+
+// Adding two numbers
+let sum = num1 + num2;
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
--- add.py
+++
@@ -1,3 +0,0 @@
-x = int(input("Please enter x"))
-y = int(input("Please enter y"))
-z = print(x+y)
