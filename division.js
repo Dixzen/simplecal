96e52e7a4b69c94b1f55b2113dec224d1020fd2c
parijatkasbekar <parijatkasbekar@gmail.com>
2023-02-23 15:51:01 +0530

    Division

---
+++ division.js
@@ -0,0 +1,4 @@
+let num1 = prompt("Enter the first number: ");
+let num2 = prompt("Enter the second number: ");
+let result = parseFloat(num1) / parseFloat(num2);
+console.log("The result of the division is: " + result);
