785496cc8e48145c606e34c45ad229c2571edff8
Palak807 <88302656+Palak807@users.noreply.github.com>
2023-02-23 15:22:28 +0530

    Update multiply.js

--- multiply.js
+++ multiply.js
@@ -3,5 +3,5 @@ let num1 = parseFloat(prompt("Enter the first number:"));
 let num2 = parseFloat(prompt("Enter the second number:"));
 
 // Adding two numbers
-let sum = num1 * num2;
+let multiply = num1 * num2;
 console.log("The product of " + num1 + " and " + num2 + " is: " + multiply);
