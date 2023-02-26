1e4c72ffb327d7ca2e87bf8417b3fb09d1808d66
Palak807 <palak.sahu20@st.niituniversity.in>
2023-02-26 18:34:59 +0530

    new comments

--- division.js
+++ division.js
@@ -1,4 +1,6 @@
-let num1 = prompt("Enter the first number: ");
-let num2 = prompt("Enter the second number: ");
-let result = parseFloat(num1) / parseFloat(num2);
-console.log("The result of the division is: " + result);
+function division(a, b) {
+    return a/b;
+  }
+  
+  export default division;
+  
