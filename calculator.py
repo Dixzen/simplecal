afdc6f914bc4204198f6f7df133de93afdf0b925
Palak807 <palak.sahu20@st.niituniversity.in>
2023-02-28 18:40:27 +0530

    Palak's commit

---
+++ calculator.py
@@ -0,0 +1,34 @@
+from addition import add
+from subtraction import subtract
+from multiplication import multiply
+from division import divide
+from power import power
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+
+elif choice == '5':
+    print(num1, "^", num2, "=", power(num1, num2))
+
+else:
+    print("Invalid input")
---
+++ power.py
@@ -0,0 +1,2 @@
+def power(a, b):
+    return a ^ b
